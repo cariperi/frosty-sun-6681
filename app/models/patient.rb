84b334f934da05f9fe784cee2fa13241587e33d5
Caroline Peri <caroline.anne.peri@gmail.com>
2023-04-24 12:56:30 -0400

    feat: add class method on patient class to get all adult patients and sort by name

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,4 +1,8 @@
 class Patient < ApplicationRecord
   has_many :patient_doctors
   has_many :doctors, through: :patient_doctors
+
+  def self.adult_patients_ordered
+    where('age > ?', 18).order(patient_name: :asc)
+  end
 end
