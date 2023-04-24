6db73d729d1c952262753c136a625a9c1b979a18
Caroline Peri <caroline.anne.peri@gmail.com>
2023-04-24 12:35:39 -0400

    feat: add get record method to patient doctor model

--- app/models/patient_doctor.rb
+++ app/models/patient_doctor.rb
@@ -1,4 +1,8 @@
 class PatientDoctor < ApplicationRecord
   belongs_to :patient
   belongs_to :doctor
+
+  def self.get_record(patient_id, doctor_id)
+    where(["patient_id = ? AND doctor_id = ?", patient_id, doctor_id]).first
+  end
 end
